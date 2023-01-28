093fa04ce6cd04a5bc545482c9ad807a40112297
a60231023 <ayushtiwari6061@gmail.com>
2023-01-28 19:08:02 +0530

    login and logout controller done

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -60,5 +60,57 @@ export const signup = asyncHandler(async (req, res) =>{
 @description User signin controller for signing the user in
 @parameters email, password
 @return User Objects
-
 */
+
+export const login = asyncHandler( async (req, res) => {
+    const {email, password } = req.body;
+    if(!email || !password){
+        throw new CustomError('Please fill all the fields', 400);
+    }
+    
+    //you have to compare the password but select is false so you do .select + which will override the false
+
+    const user = User.findOne({email}).select("+password");
+
+    if(!user){
+        throw new CustomError('Invalid credentials', 400);
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(isPasswordMatched){
+        const token = user.getJwtToken();
+        user.password = undefined;
+        res.cookei("token", token, cookieOptions)
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+    throw new CustomError('Invalid credentials ', 400);
+});
+
+/* 
+
+@Logout
+@route http://localhost:4000/api/auth/logout
+@description user logging out
+@parameters 
+@return success message
+*/
+
+
+export const logout = asyncHandler(async (req, res) => {
+    // res.clearCookie();
+    
+    res.cookie("token", null, {
+        expires : new Date(Date.now()),
+        httpOnly: true
+    });
+    res.status(200).json({
+        success: true,
+        message: "You have logged out"
+    })
+
+});
