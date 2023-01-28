647de3753a6c843bc9e802fc26d82148cfac84e9
a60231023 <ayushtiwari6061@gmail.com>
2023-01-28 12:49:01 +0530

    added collection and product schema

---
+++ models/prodcutSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : [true, "Please provide a category name"],
+            trim : true,
+            maxLength : [120, "collection name cannote be more than 120 characters"]
+        }
+    },
+    {
+        timestamps : true
+    }
+);
+
+export default mongoose.model("Product", productSchema)
