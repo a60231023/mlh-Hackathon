8580fa0c43cd9c5a570e5a9e18c745556e771257
a60231023 <ayushtiwari6061@gmail.com>
2023-01-29 11:22:18 +0530

    added forgot password with email send functionality

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/userSchema.js'
 import asyncHandler from '../services/asyncHandler.js'
 import CustomError from '../utils/customError.js'
+import mailHelper from '../utils/mailHelper.js';
 
 //3days expiry
  
@@ -118,10 +119,47 @@ export const logout = asyncHandler(async (req, res) => {
 
 /* 
 
-@Logout
-@route http://localhost:4000/api/auth/logout
-@description user logging out
-@parameters 
-@return success message
+@forgot passwrord
+@route http://localhost:4000/api/auth/password/forgot
+@description user will submit and we will generate the token
+@parameters  email
+@return success message - emailsent
 
 */
+
+export const forgotPassword = asyncHandler( async (req, res) => {
+    const {email} = req.body;
+    const user = await User.findOne({email})
+    if(!user){
+        throw new CustomError('User not found', 404)
+    }
+    const resetToken = user.generateForgotPasswordToken();
+    
+    //every time you do save, the database will check for each validation so you don't want to do that
+
+    await user.save({validateBeforeSave: false});
+
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/auth/password/reset${resetToken}`
+    const text = `Your password reset url is \n]n ${resetUrl}\n\n`
+
+    try {
+        await mailHelper({
+            email: user.email,
+            subject: "Password reset email",
+            text: text
+        });
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user..email}`
+        })
+    } catch (error) {
+        //roll-back as message not sent so forgot password token which was saved in the db has to be changed back
+
+        user.forgotPasswordToken = undefined;
+        user.forgotPasswordExpiry = undefined;
+        await user.save({validateBeforeSave: false});
+
+        throw new CustomError(err.message || 'Email sent failure', 500);
+
+    }
+})
--- config/index.js
+++ config/index.js
@@ -10,7 +10,8 @@ const config = {
     SMTP_MAIL_HOST : process.env.SMTP_MAIL_HOST,
     SMTP_MAIL_PORT : process.env.SMTP_MAIL_PORT,
     SMTP_MAIL_USERNAME : process.env.SMTP_MAIL_USERNAME,
-    SMTP_MAIL_PASSWORD : process.env.SMTP_MAIL_PASSWORD
+    SMTP_MAIL_PASSWORD : process.env.SMTP_MAIL_PASSWORD,
+    SMTP_MAIL_EMAIL : process.env.SMTP_MAIL_EMAIL
 
 }
 
